b9b170663b9de2fa0ef7a59cdab7313d022cd351
Al Nahian <nahian33@yahoo.com>
2014-06-09 22:47:29 +0600

    add devise to gemfile, generate devise user and migrate, create home controller with index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningDevise::Application.config.secret_token = 'd77eb7d7bfa58b168341cf27d299d7ec97fca0986aa305ac1b873764aa5ffb5df6cc86ca4f6c22f84add9a5e942399c0b40452131f265b511dce654d8615d75e'
