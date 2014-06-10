1799bb94a11badfa5691a9b8a418531745ce030c
Al Nahian <nahian33@yahoo.com>
2014-06-10 14:21:04 +0600

    sign in with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,27 @@
 class User < ActiveRecord::Base
+
+  # setup attr_accessor
+  attr_accessor :signin
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:signin]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :signin, :username, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  
+
+  
+  validates :username, :uniqueness => {:case_sensitive => false}
+  
+    def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if signin = conditions.delete(:signin)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => signin.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
 end
